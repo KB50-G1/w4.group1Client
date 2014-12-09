6682f2d4d59134092f4858e21d0229dec912fc9d
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2014-12-09 15:21:57 +0100

    Added none working spinner

--- app/src/main/java/pidal/alfonso/w4group1client/EditOfficeActivity.java
+++ app/src/main/java/pidal/alfonso/w4group1client/EditOfficeActivity.java
@@ -3,20 +3,23 @@ package pidal.alfonso.w4group1client;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import pidal.alfonso.w4group1client.Models.Company;
 import pidal.alfonso.w4group1client.Models.Office;
 import pidal.alfonso.w4group1client.Models.OfficeType;
 
 
-public class EditOfficeActivity extends Activity {
+public class EditOfficeActivity extends Activity{
 
     private Office office;
 
     private EditText phoneNumber;
     private EditText address;
-    private EditText officeType;
+    private Spinner officeType;
 
     int officeIDintent;
 
@@ -27,7 +30,17 @@ public class EditOfficeActivity extends Activity {
 
         phoneNumber = (EditText) findViewById(R.id.phone_number_edit_text);
         address = (EditText) findViewById(R.id.address_edit_text);
-        officeType = (EditText) findViewById(R.id.office_type_edit_text);
+        officeType = (Spinner) findViewById(R.id.office_type_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.office_types,
+                android.R.layout.simple_spinner_item
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        officeType.setAdapter(adapter);
+
+        officeType.setOnItemClickListener(new SpinnerActivity());
 
         officeIDintent = getIntent().getIntExtra(OfficeListActivity.IDintent, 0);
         if (officeIDintent != 0) {
@@ -36,7 +49,7 @@ public class EditOfficeActivity extends Activity {
 
             phoneNumber.setText(Integer.toString(office.getPhoneNumber()));
             address.setText(office.getAddress());
-            officeType.setText(office.getOfficeType().toString());
+            //officeType.setText(office.getOfficeType().toString());
         }
 
     }
@@ -45,17 +58,19 @@ public class EditOfficeActivity extends Activity {
         if (officeIDintent != 0) {
             office.setPhoneNumber(Integer.parseInt(phoneNumber.getText().toString()));
             office.setAddress(address.getText().toString());
-            office.setOfficeType(OfficeType.valueOf(officeType.getText().toString()));
+            //office.setOfficeType(OfficeType.valueOf(officeType.getText().toString()));
             OfficeListActivity.officeHelper.updateOffice(office);
         } else {
             office = new Office();
 
             office.setPhoneNumber(Integer.parseInt(phoneNumber.getText().toString()));
             office.setAddress(address.getText().toString());
-            office.setOfficeType(OfficeType.valueOf(officeType.getText().toString()));
+            //office.setOfficeType(OfficeType.valueOf(officeType.getText().toString()));
             office.setCompany(new Company(1, "Alber", "https://www.google.com"));
             OfficeListActivity.officeHelper.addOffice(office);
         }
         this.finish();
     }
+
+
 }
